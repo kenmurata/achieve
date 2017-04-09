d5036d02a7bb227a0f37b98c77653f86c5245dd7
Ken Murata <k_murata@nttpc.co.jp>
2017-04-09 23:30:21 +0000

    first commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,34 @@
+class ContactsController < ApplicationController
+  def index
+    redirect_to new_contact_path
+  end
+  
+  def new
+    if params[:back]
+      @contact = Contact.new(contact_params)
+    else
+      @contact = Contact.new
+    end
+  end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      #redirect_to new_contact_path, notice: "お問い合わせありがとうございました！"
+      redirect_to root_path, notice: "お問い合わせありがとうございました！"
+    else
+      render 'new'
+    end
+    #redirect_to new_contact_path
+  end
+  
+  def confirm
+    @contact = Contact.new(contact_params)
+    render :new if @contact.invalid?
+  end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :content)
+    end
+end
