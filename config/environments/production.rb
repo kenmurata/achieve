994160d6646ea7f90805d4bf8d705d06b8333d98
Ken Murata <k_murata@nttpc.co.jp>
2017-05-08 10:08:24 +0000

    DIVE11 syusei

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'immense-savannah-36031' }
+  config.action_mailer.default_url_options = { host: 'immense-savannah-36031.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = 
   {
