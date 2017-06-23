4a843aa8dc6f07bbec6fa0be38a31fddb2bd9e50
Ken Murata <k_murata@nttpc.co.jp>
2017-06-23 02:33:30 +0000

    DIVE20-2 SSH syusei

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '52.69.133.208', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/home/ubuntu/workspace/memo_and_back/aws_ssh_key'
+set :ssh_options, keys: '/home/ubuntu/.ssh/id_rsa
